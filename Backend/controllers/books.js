df5554368467541baa68635077f0dec13e323e3e
Yaser Akkurt <yasi.akurt@gmail.com>
2023-12-24 19:11:55 +0200

    modele bookSchema da yeni dört fazla bilgi

--- Backend/controllers/books.js
+++ Backend/controllers/books.js
@@ -1,8 +1,14 @@
 const Book = require('../models/Book')
 
 const getAllBooks = async (req, res) => {
+    //skip - atla demek
+    //limit - şu kadar göster demek
+    //query ile page ve limit alıp skip bunlarla hesaplatılıyor
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
 
-    const books = await Book.find({});
+    const books = await Book.find({}).skip(skip).limit(limit);
     return res.status(201).send({ message: "Başarılı", data: books, count: books.length })
 
 }
@@ -22,7 +28,14 @@ const getBook = async (req, res) => {
 
 const addBook = async (req, res) => {
 
-    const newBook = { title: req.body.title, author: req.body.author, publishYear: req.body.publishYear }
+    const newBook = {   title: req.body.title, 
+                        author: req.body.author, 
+                        publishYear: req.body.publishYear, 
+                        barcode: req.body.barcode, 
+                        publisher: req.body.publisher, 
+                        stock: req.body.stock, 
+                        price: req.body.price 
+                    }
     const book = await Book.create(newBook)
     return res.status(201).send({ message: "Başarılı", data: book })
 }
--- Backend/models/Book.js
+++ Backend/models/Book.js
@@ -4,17 +4,40 @@ const bookSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, "isim boş bırakılamaz"],
+        maxlength: [100, "isim en fazla 100 karakter olabilir"]
     },
     author: {
         type: String,
         required: [true, "yazar boş bırakılamaz"],
+        maxlength: [100, "yazar en fazla 100 karakter olabilir"]
     },
     publishYear: {
-        type: Number
+        type: Number,
+        min: [1453, "Yayın tarihi 1453'ten sonra olmalıdır"],
+        max: [2021, "Yayın tarihi 2021'den önce olmalıdır"]
+    },
+    barcode: {
+        type: String,
+        required: [true, "barkod boş bırakılamaz"],
+        unique: true
+    },
+    publisher: {
+        type: String,
+        required: [true, "yayınevi boş bırakılamaz"],
+        maxlength: [50, "yayınevi en fazla 50 karakter olabilir"]
+    },
+    stock: {
+        type: Number,
+        required: [true, "stok boş bırakılamaz"],
+        min: [1, "Stok birden az olamaz"]
+    },
+    price: {
+        type: Number,
+        required: [true, "fiyat boş bırakılamaz"],
+        min: [5, "Fiyat beşten az olamaz"]
     }
 }, {
     timestamps: true
 });
 
 module.exports = mongoose.model('Book', bookSchema);
-
